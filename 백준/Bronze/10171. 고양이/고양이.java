8c5b88a0a174186a0fc8d8a4fbe69e14411c0402
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-02 12:14:20 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+		
+		System.out.println(sb);
+	}
+}
