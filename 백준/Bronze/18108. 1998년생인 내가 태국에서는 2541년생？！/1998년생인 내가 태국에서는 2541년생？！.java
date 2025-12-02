aff9073e5d81df266ff89790f32cfcd38ac9e33a
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-02 11:22:47 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 184 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int Seogi = sc.nextInt();
+        int c = 2541-1998;
+        int result = Seogi - c;
+        System.out.println(result);
+    }
+}
