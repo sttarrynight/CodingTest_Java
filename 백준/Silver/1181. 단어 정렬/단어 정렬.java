02f480c3c8f3b867e61d98184cf42cf20072ee4a
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-04 09:36:49 +0900

    [Silver V] Title: 단어 정렬, Time: 1864 ms, Memory: 25792 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		List<String> list = new ArrayList<>();
+		
+		while(N-->0) {
+			String str = br.readLine();
+			if(!list.contains(str)) {
+				list.add(str);
+			}
+		}
+		
+		list.sort((x,y)->x.compareTo(y));
+		list.sort((x,y)->x.length()-y.length());
+		
+		for(String s : list) {
+			sb.append(s).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
