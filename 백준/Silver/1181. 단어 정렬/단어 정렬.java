9a5be4b7debbab67f09ca161425ab3e12bc26e12
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-08 10:24:07 +0900

    [Silver V] Title: 단어 정렬, Time: 344 ms, Memory: 27100 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,36 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
 		StringBuilder sb = new StringBuilder();
 		
-		List<String> list = new ArrayList<>();
+		int N = Integer.parseInt(br.readLine());
+		Set<String> set = new HashSet<>();
 		
 		while(N-->0) {
-			String str = br.readLine();
-			if(!list.contains(str)) {
-				list.add(str);
-			}
+			set.add(br.readLine());
 		}
 		
-		list.sort((x,y)->{
-            if(x.length() != y.length()) return x.length()-y.length();
-            else return x.compareTo(y);
-        });
+		String[] str;
+		str = set.toArray(new String[0]);
 		
-		for(String s : list) {
+		Arrays.sort(str, (s1,s2)->{
+			if(s1.length()!= s2.length()) return s1.length() - s2.length();
+			else return s1.compareTo(s2);
+		});
+		
+		for(String s : str) {
 			sb.append(s).append("\n");
 		}
+		
 		System.out.println(sb);
 	}
+
 }
