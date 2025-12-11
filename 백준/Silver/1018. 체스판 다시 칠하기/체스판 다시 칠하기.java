6aac2d7c70074d1336d7b6ee7ccba7e9aed2fc51
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-11 12:27:02 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 15680 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -25,60 +25,37 @@ public class Main {
 		
 		for(int i=0; i<N-8+1; i++) {
 			for(int j=0; j<M-8+1; j++) {
-				char compChar = board[i].charAt(j);
-				for(int k=0;k<2;k++) {
-					int cnt = 0;
-					if(k==1) {
-						compChar = compChar=='B'?'W':'B';
-					}
-					row = i;
-					col=j;
-					if(compChar=='B') {
-						while(row < i+8) {
-							while(col < j+8) {
-								if(row%2 == i%2) {
-									if(col%2 == j%2) {
-										cnt += board[row].charAt(col)=='B'?0:1;
-									} else {
-										cnt += board[row].charAt(col)=='W'?0:1;
-									}
-								} else {
-									if(col%2 == j%2) {
-										cnt += board[row].charAt(col)=='W'?0:1;
-									} else {
-										cnt += board[row].charAt(col)=='B'?0:1;
-									}
-								}
-								col++;
+				int cntB = 0;
+				int cntW = 0;
+				row = i;
+				col=j;
+				while(row < i+8) {
+					while(col < j+8) {
+						if(row%2 == i%2) {
+							if(col%2 == j%2) {
+								cntB += board[row].charAt(col)=='B'?0:1;
+								cntW += board[row].charAt(col)=='W'?0:1;
+							} else {
+								cntB += board[row].charAt(col)=='W'?0:1;
+								cntW += board[row].charAt(col)=='B'?0:1;
 							}
-							col = j;
-							row++;
-						}
-					} else {
-						while(row < i+8) {
-							while(col < j+8) {
-								if(row%2 == i%2) {
-									if(col%2 == j%2) {
-										cnt += board[row].charAt(col)=='W'?0:1;
-									} else {
-										cnt += board[row].charAt(col)=='B'?0:1;
-									}
-								} else {
-									if(col%2 == j%2) {
-										cnt += board[row].charAt(col)=='B'?0:1;
-									} else {
-										cnt += board[row].charAt(col)=='W'?0:1;
-									}
-								}
-								col++;
+						} else {
+							if(col%2 == j%2) {
+								cntB += board[row].charAt(col)=='W'?0:1;
+								cntW += board[row].charAt(col)=='B'?0:1;
+							} else {
+								cntB += board[row].charAt(col)=='B'?0:1;
+								cntW += board[row].charAt(col)=='W'?0:1;
 							}
-							col = j;
-							row++;
 						}
+						col++;
 					}
-					if(cnt<min) {
-						min = cnt;
-					}
+					col = j;
+					row++;
+				}
+				int cnt = cntB>=cntW? cntW : cntB;
+				if(cnt<min) {
+					min = cnt;
 				}
 			}
 		}
