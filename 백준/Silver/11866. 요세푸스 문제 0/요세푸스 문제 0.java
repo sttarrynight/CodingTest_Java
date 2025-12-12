61c0a3d412a7c3b621c7f3e1a7ee342f6e05dac0
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-12 14:28:59 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		List<Integer> list = new LinkedList<>();
+		
+		for(int i=1; i<=N; i++) {
+			list.add(i);
+		}
+		
+		List<Integer> answer = new ArrayList<>();
+		int rmIdx = K-1;
+		int sizeNow = list.size();
+		
+		for(int i=0; i<N; i++) {
+			while(rmIdx >= sizeNow) {
+				rmIdx -= sizeNow;
+			}
+			answer.add(list.get(rmIdx));
+			list.remove(rmIdx);
+			rmIdx--;
+			rmIdx += K;
+			sizeNow = list.size();
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		for(int i=0; i<answer.size(); i++) {
+			sb.append(answer.get(i));
+			sb.append(i!=answer.size()-1 ? ", " : ">");
+		}
+		System.out.println(sb);
+	}
+}
