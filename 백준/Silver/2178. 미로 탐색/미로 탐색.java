d2719f9f18df4131d25dca33b41bd6d907621899
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-23 22:49:31 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16328 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,23 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int[][] miro;
+	static class C {
+		int x, y, z; // x:행, y:열, z:최단거리
+		
+		public C(int x, int y, int z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+	
 	static int N, M;
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		miro = new int[N][M];
+		int[][] miro = new int[N][M];
 		
 		for(int i=0; i<N; i++) {
 			String[] s = br.readLine().split("");
@@ -27,31 +35,35 @@ public class Main {
 				miro[i][j] = Integer.parseInt(s[j]);
 			}
 		}
+		Deque<C> queue = new ArrayDeque<>();
 		
-		bfs();
-	}
-
-	private static void bfs() {
-		Deque<int[]> queue = new ArrayDeque<>();
 		int[] di = {0,0,1,-1};
 		int[] dj = {-1,1,0,0};
-		queue.add(new int[] {0,0});
-		miro[0][0] = 2;
+		
+		miro[0][0] = -1;
+		
+		queue.add(new C(0,0,1));
+		
 		while(!queue.isEmpty()) {
-			int cur[] = queue.poll();
-			for(int x=0; x<4; x++) {
-				int ni = cur[0] + di[x];
-				int nj = cur[1] + dj[x];
-				if(ni==N-1 && nj==M-1) {
-					System.out.println(miro[cur[0]][cur[1]]);
-					return;
-				}
-				if(ni < 0 || ni > N-1 || nj < 0 || nj > M-1) continue;
+			C c = queue.poll();
+			
+			if(c.x==N-1 && c.y==M-1) {
+				System.out.println(c.z);
+				return;
+			}
+			
+			for(int k=0; k<4; k++) {
+				int ni = c.x + di[k];
+				int nj = c.y + dj[k];
+				
+				if(ni>N-1 || ni<0 || nj>M-1 || nj<0) continue;
 				if(miro[ni][nj]==1) {
-					miro[ni][nj] = miro[cur[0]][cur[1]] + 1;
-					queue.add(new int[] {ni,nj});
+					miro[ni][nj] = -1;
+					queue.add(new C(ni,nj,c.z+1));
 				}
 			}
 		}
+		
 	}
+
 }
