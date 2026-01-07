18725bc589bf00aab986a7b59b325ccac43b15e2
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-01-07 09:01:48 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 956 ms, Memory: 96956 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static int R;
+	static LinkedList<Integer>[] adj;
+	static boolean[] visited;
+	static int[] seq;
+	static int cnt;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		adj = new LinkedList[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			adj[i] = new LinkedList();
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			adj[u].add(v);
+			adj[v].add(u);
+		}
+		
+		visited = new boolean[N+1];
+		seq = new int[N+1];
+		cnt = 1;
+		
+		bfs(R);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++) {
+			sb.append(seq[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static void bfs(int r) {
+		Deque<Integer> queue = new ArrayDeque<>();
+		visited[r] = true;
+		queue.add(r);
+		seq[r] = cnt++;
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+			Collections.sort(adj[cur]);
+			for(int i : adj[cur]) {
+				if(!visited[i]) {
+					visited[i] = true;
+					queue.add(i);
+					seq[i] = cnt++;
+				}
+			}
+		}
+	}
+}
