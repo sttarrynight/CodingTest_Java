56a876e68ec72cddfec44f80c9d4c1cedf514407
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-01-11 16:21:03 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 616 ms, Memory: 44912 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		Integer[] A;
+		Integer[] B;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(T-->0) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			A = new Integer[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				A[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(A, (x,y)->y-x);
+			
+			B = new Integer[M];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<M; i++) {
+				B[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(B);
+			
+			int sum = 0;
+			for(int i=0; i<N; i++) {
+				int left = 0;
+				int right = B.length-1;
+				int idx = -1;
+				
+				while(left<=right) {
+					int mid = (left+right)/2;
+					if(B[mid]<A[i]) {
+						idx = mid;
+						left = mid+1;
+					} else {
+						right = mid-1;
+					}
+				}
+				if(idx!=-1) {
+					sum += idx+1;
+				}
+			}
+			sb.append(sum).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
