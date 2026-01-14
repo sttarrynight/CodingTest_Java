c018562ef9987168cf92c7f6b6c29a76777c9573
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-01-14 23:18:30 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 96 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -3,10 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	static int code1;
-	static int code2;
-	static int[] f;
 
 	public static void main(String[] args) throws IOException {
 		
@@ -14,30 +10,17 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 		
-		fib(n);
-		f = new int[n+1];
-		fibonacci(n);
+		int[] f = new int[n+1];
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append(code1).append(" ").append(code2);
-		System.out.println(sb);
-	}
-
-	private static int fibonacci(int n) {
 		f[1] = 1; f[2] = 1;
+		
 		for(int i=3; i<=n; i++) {
 			f[i] = f[i-1] + f[i-2];
-			code2++;
 		}
-		return f[n];
-	}
-
-	private static int fib(int n) {
-		if(n==1 || n==2) {
-			code1++;
-			return 1;
-		}
-		else return fib(n-1) + fib(n-2);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(f[n]).append(" ").append(n-2);
+		System.out.println(sb);
 	}
 
 }
