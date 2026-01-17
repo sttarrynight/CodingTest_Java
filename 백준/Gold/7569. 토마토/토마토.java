98ecf0ebaddc41d57d30f696407b5ff6cbad2351
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-01-17 15:32:35 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 105032 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -26,22 +26,32 @@ public class Main {
 		H = Integer.parseInt(st.nextToken());
 		
 		tomato = new int[H][N][M];
+		Deque<int[]> queue = new ArrayDeque<>();
 		
 		for(int i=0; i<H; i++) {
 			for(int j=0; j<N; j++) {
 				st = new StringTokenizer(br.readLine());
 				for(int k=0; k<M; k++) {
 					tomato[i][j][k] = Integer.parseInt(st.nextToken());
+					if(tomato[i][j][k]==1) {
+						queue.add(new int[] {i,j,k});
+					}
 				}
 			}
 		}
 		
-		for(int i=0; i<H; i++) {
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<M; k++) {
-					if(tomato[i][j][k]==1) {
-						bfs(i,j,k);
-					}
+		while(!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			for(int x=0; x<6; x++) {
+				int ni = cur[0] + di[x];
+				int nj = cur[1] + dj[x];
+				int nk = cur[2] + dk[x];
+				
+				if(ni < 0 || ni > H-1 || nj < 0 || nj > N-1 || nk < 0 || nk > M-1) continue;
+				
+				if(tomato[ni][nj][nk]==0) {
+					tomato[ni][nj][nk] = tomato[cur[0]][cur[1]][cur[2]] + 1;
+					queue.add(new int[] {ni,nj,nk});
 				}
 			}
 		}
@@ -63,25 +73,5 @@ public class Main {
 		}
 		
 		System.out.println(max-1);
-	}
-
-	private static void bfs(int i, int j, int k) {
-		Deque<int[]> queue = new ArrayDeque<>();
-		queue.add(new int[] {i,j,k});
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			for(int x=0; x<6; x++) {
-				int ni = cur[0] + di[x];
-				int nj = cur[1] + dj[x];
-				int nk = cur[2] + dk[x];
-				
-				if(ni < 0 || ni > H-1 || nj < 0 || nj > N-1 || nk < 0 || nk > M-1) continue;
-				
-				if(tomato[ni][nj][nk]==0 || (tomato[ni][nj][nk]>1 && tomato[ni][nj][nk]>tomato[cur[0]][cur[1]][cur[2]] + 1)) {
-					tomato[ni][nj][nk] = tomato[cur[0]][cur[1]][cur[2]] + 1;
-					queue.add(new int[] {ni,nj,nk});
-				}
-			}
-		}
 	}
 }
