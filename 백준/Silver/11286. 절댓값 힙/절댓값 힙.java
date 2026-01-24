d343073bb2916df791801dd90f55333ca890a87b
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-01-24 09:52:24 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 25264 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->{
+			if(Math.abs(x) != Math.abs(y)) {
+				return Math.abs(x) - Math.abs(y);
+			} else {
+				return x-y;
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(N-->0) {
+			int x = Integer.parseInt(br.readLine());
+			if(x==0) {
+				sb.append(pq.isEmpty()?0:pq.poll()).append("\n");
+			} else {
+				pq.add(x);
+			}
+		}
+		System.out.println(sb);
+	}
+}
