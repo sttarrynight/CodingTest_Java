51427694c326ba0d95289a6976c06133e4014158
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-02-07 01:14:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 96 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		long[] dp = new long[1001];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i=3; i<=N; i++) {
+			dp[i] = dp[i-1]%10007 + dp[i-2]%10007;
+		}
+		
+		System.out.println(dp[N]%10007);
+	}
+}
