630c93ba6431f21b03f71b7d80a907ba5b318b9a
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-02-12 17:09:15 +0900

    [Gold IV] Title: 플로이드, Time: 356 ms, Memory: 42768 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,88 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int M;
-	static List<Bus>[] adj;
-	
-	public static class Bus {
-		int stop;
-		int pay;
-		
-		public Bus(int stop, int pay) {
-			this.stop = stop;
-			this.pay = pay;
-		}
-	}
-	
+
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st;
 		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 		
-		adj = new ArrayList[N+1];
+		int[][] nodes = new int[N+1][N+1];
 		
 		for(int i=1; i<=N; i++) {
-			adj[i] = new ArrayList<>();
+			Arrays.fill(nodes[i], 100000001);
+			nodes[i][i] = 0;
 		}
 		
-		for(int i=0; i<M; i++) {
+		for(int i=1; i<=M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int v = Integer.parseInt(st.nextToken());
 			int u = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			adj[v].add(new Bus(u,w));
+			nodes[v][u] = Math.min(nodes[v][u],w);
 		}
 		
-		int[][] distance = new int[N+1][N+1];
-		
-		for(int i=1; i<=N; i++) {
-			Arrays.fill(distance[i], Integer.MAX_VALUE);
-		}
-		
-		PriorityQueue<Bus> pq;
-		
-		for(int i=1; i<=N; i++) {
-			distance[i][i] = 0;
-			
-			pq = new PriorityQueue<>((b1,b2)->{
-				return b1.pay - b2.pay;
-			});
-			pq.add(new Bus(i,distance[i][i]));
-			
-			while(!pq.isEmpty()) {
-				Bus current = pq.poll();
-				
-				if(distance[i][current.stop] < current.pay) continue;
-				
-				for(Bus next : adj[current.stop]) {
-					if(distance[i][current.stop]+next.pay<distance[i][next.stop]) {
-						distance[i][next.stop] = distance[i][current.stop]+next.pay;
-						pq.add(new Bus(next.stop,distance[i][next.stop]));
-					}
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					nodes[i][j] = Math.min(nodes[i][j], nodes[i][k] + nodes[k][j]);
 				}
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		
-		for(int i=1;i<=N;i++) {
+		for(int i=1; i<=N; i++) {
 			for(int j=1; j<=N; j++) {
-				if(distance[i][j]==Integer.MAX_VALUE) {
-					sb.append(0).append(" ");
+				if(nodes[i][j]!=100000001) {
+					sb.append(nodes[i][j]).append(" ");
 				} else {
-					sb.append(distance[i][j]).append(" ");
+					sb.append(0).append(" ");
 				}
 			}
 			sb.append("\n");
@@ -90,4 +53,5 @@ public class Main {
 		
 		System.out.println(sb);
 	}
+
 }
