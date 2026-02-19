52b6e53c9062917b75c17004cd509d4d5b60e247
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-02-19 10:23:12 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 16468 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N,M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N+1];
+		
+		dfs(0);
+		
+		System.out.println(sb);
+	}
+
+	private static void dfs(int depth) {
+		if(depth==M) {
+			for(int j=1; j<=M; j++) {
+				sb.append(arr[j]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=1; i<=N; i++) {
+			if(arr[depth]<=i) {
+				arr[depth+1] = i;
+				dfs(depth+1);
+			}
+		}
+	}
+
+}
