0a063320b9218ee1b109c0d7e995bdab2ce60a56
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-02-20 10:47:15 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N+1];
+		
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		for(int i=2; i<=N; i++) {
+			arr[i] = arr[i-1] + arr[i-2];
+		}
+		
+		System.out.println(arr[N]);
+	}
+
+}
