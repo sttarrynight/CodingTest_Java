383cec6024e5151940419282c810ba828aab1d04
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-03-04 16:13:57 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 19724 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-// 백준 1238번 파티 풀이1 - 다익스트라 알고리즘
+// 백준 1238번 파티 풀이3 - 다익스트라 알고리즘(2번만으로)
 public class Main {
 	
 	static class Node {
@@ -23,9 +23,11 @@ public class Main {
 		int X = Integer.parseInt(st.nextToken());
 		
 		List<Node>[] adj = new ArrayList[N+1];
+		List<Node>[] adjReverse = new ArrayList[N+1];
 		
 		for(int i=1; i<=N; i++) {
 			adj[i] = new ArrayList<>();
+			adjReverse[i] = new ArrayList<>();
 		}
 		
 		for(int i=0; i<M; i++) {
@@ -34,37 +36,65 @@ public class Main {
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			adj[u].add(new Node(v,w));
+			adjReverse[v].add(new Node(u,w));
 		}
 		
-		int[][] distance = new int[N+1][N+1];
+		int[] distance = new int[N+1];
 		
 		PriorityQueue<Node> pq = new PriorityQueue<>((n1,n2)->{
 			return n1.weight - n2.weight;
 		});
 		
-		for(int i=1; i<=N; i++) {
-			Arrays.fill(distance[i],100000001);
-			distance[i][i] = 0;
-			pq.add(new Node(i,distance[i][i]));
-			while(!pq.isEmpty()) {
-				Node cur = pq.poll();
-				
-				if(distance[i][cur.idx]<cur.weight) continue;
-				
-				for(Node next : adj[cur.idx]) {
-					int nextWeight = distance[i][cur.idx]+next.weight;
-					if(nextWeight < distance[i][next.idx]) {
-						distance[i][next.idx] = nextWeight;
-						pq.add(new Node(next.idx,distance[i][next.idx]));
-					}
+		Arrays.fill(distance,100000001);
+		distance[X] = 0;
+		
+		pq.add(new Node(X,distance[X]));
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if(distance[cur.idx]<cur.weight) continue;
+			
+			for(Node next : adj[cur.idx]) {
+				int nextWeight = distance[cur.idx]+next.weight;
+				if(nextWeight < distance[next.idx]) {
+					distance[next.idx] = nextWeight;
+					pq.add(new Node(next.idx,distance[next.idx]));
+				}
+			}
+		}
+		
+		int[] distance2 = new int[N+1];
+		
+		pq = new PriorityQueue<>((n1,n2)->{
+			return n1.weight - n2.weight;
+		});
+		
+		Arrays.fill(distance2,100000001);
+		distance2[X] = 0;
+		
+		pq.add(new Node(X,distance2[X]));
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if(distance2[cur.idx]<cur.weight) continue;
+			
+			for(Node next : adjReverse[cur.idx]) {
+				int nextWeight = distance2[cur.idx]+next.weight;
+				if(nextWeight < distance2[next.idx]) {
+					distance2[next.idx] = nextWeight;
+					pq.add(new Node(next.idx,distance2[next.idx]));
 				}
 			}
 		}
 		
 		int max = 0;
+		
 		for(int i=1; i<=N; i++) {
-			max = Math.max(max, distance[i][X] + distance[X][i]);
+			max = Math.max(max, distance[i] + distance2[i]);
 		}
+		
 		System.out.println(max);
 	}
 
