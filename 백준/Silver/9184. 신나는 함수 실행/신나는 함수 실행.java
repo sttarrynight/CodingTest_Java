4aabf8c29d112b8fed6b27e6861830f08dbfe576
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-03-10 00:15:45 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 316 ms, Memory: 29088 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int[][][] abc = new int[21][21][21];
+	
+	static int w(int a, int b, int c) {
+		if(a<=0 || b<=0 || c<=0) {
+			return 1;
+		}
+		
+		if(a>20 || b>20 || c>20) {
+			return w(20,20,20);
+		}
+		
+		if(abc[a][b][c] != 0) return abc[a][b][c];
+		
+		if(a<b && b<c) {
+			abc[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c);
+			return abc[a][b][c];
+		}
+		else {
+			abc[a][b][c] = w(a-1,b,c) + w(a-1,b-1,c) + w(a-1,b,c-1) - w(a-1,b-1,c-1);
+			return abc[a][b][c];
+		}
+	}
+
+	public static void main(String[] args) throws IOException  {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			if(a==-1 && b==-1 && c==-1) break;
+			sb.append("w(" + a + ", " + b + ", " + c + ") = " + w(a,b,c)).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
