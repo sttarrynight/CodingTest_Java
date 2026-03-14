bfdbec7f3ca1277f47e537627fc3001f20da0826
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-03-14 16:05:34 +0900

    [Silver V] Title: 소수, Time: 116 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int result = 0;
+		
+		A %= B;
+		
+		for(int i=0; i<N; i++) {
+			A *= 10;
+			result = A / B;
+			A %= B;
+		}
+		
+		System.out.println(result);
+	}
+
+}
