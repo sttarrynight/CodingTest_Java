1bac3d6e22af8054900da99a4e36341f2667807f
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-03-17 08:59:54 +0900

    [Silver II] Title: 연속합, Time: 240 ms, Memory: 23896 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -20,15 +20,10 @@ public class Main {
 		int[] dp = new int[n];
 		dp[0] = arr[0];
 		
+		int max = dp[0];
 		for(int i=1; i<n; i++) {
 			dp[i] = Math.max(dp[i-1]+arr[i],arr[i]);
-		}
-		
-		int max = Integer.MIN_VALUE;
-		for(int i : dp) {
-			if(i>max) {
-				max = i;
-			}
+			max = Math.max(max,dp[i]);
 		}
 		
 		System.out.println(max);
