6ce710862d234c60c962c402b7fd8baffbc4f6dc
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-04-07 22:44:24 +0900

    [Silver II] Title: N과 M (11), Time: 348 ms, Memory: 34056 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N,M;
+	static int[] arr,nums;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		nums = new int[M];
+		sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		dfs(0);
+		
+		System.out.println(sb);
+	}
+
+	private static void dfs(int depth) {
+		if(depth==M) {
+			for(int i : nums) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		int before = 0;
+		for(int i=0; i<N; i++) {
+			if(before == arr[i]) continue;
+			before = arr[i];
+			nums[depth] = arr[i];
+			dfs(depth+1);
+		}
+	}
+
+}
