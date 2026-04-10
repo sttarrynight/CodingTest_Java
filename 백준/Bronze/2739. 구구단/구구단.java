f91c447d6144750616e205f6f7efe221e4e73dba
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-04-10 09:05:48 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<=9; i++) {
+			sb.append(N).append(" * ").append(i).append(" = ").append(N*i).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+	
+}
