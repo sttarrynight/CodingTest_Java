2d88ecf55b8baf808d3e236f99752af53f395c64
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-05-07 16:51:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.58 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,27 +1,33 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
+        
         Deque<int[]> queue = new ArrayDeque<>();
         int[] di = {0,0,-1,1};
         int[] dj = {-1,1,0,0};
+        boolean[][] visited = new boolean[n][m];
+        int[][] dist = new int[n][m];
+        
         queue.add(new int[] {0,0});
+        visited[0][0] = true;
+        dist[0][0] = 1;
+        
         while(!queue.isEmpty()){
             int[] cur = queue.poll();
             for(int x=0; x<4; x++){
                 int ni = cur[0] + di[x];
                 int nj = cur[1] + dj[x];
-                if(ni<0 || ni>n-1 || nj<0 || nj>m-1 || maps[ni][nj]==0) continue;
-                if(maps[ni][nj]==1){
-                    maps[ni][nj] += maps[cur[0]][cur[1]];
-                    queue.add(new int[] {ni,nj});
-                }
+                if(ni<0 || ni>=n || nj<0 || nj>=m) continue;
+                if(maps[ni][nj]==0 || visited[ni][nj]) continue;
+                visited[ni][nj] = true;
+                dist[ni][nj] = dist[cur[0]][cur[1]] + 1;
+                queue.add(new int[] {ni,nj});
             }
         }
-        int answer = maps[n-1][m-1]==1 ? -1 : maps[n-1][m-1];
+        int answer = visited[n-1][m-1] ? dist[n-1][m-1] : -1;
         return answer;
     }
 }
