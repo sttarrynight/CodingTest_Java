7f678c5f61824062dda24a081cb3bba2f296c63e
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-05-15 18:03:30 +0900

    [level 3] Title: 입국심사, Time: 53.83 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long left = 1L;
+        long right = (long) times[times.length-1] * n;
+        long answer = right;
+        
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long count = 0;
+            for(int t : times){
+                count += mid / t;
+            }
+            if(count>=n){
+                answer = mid;
+                right = mid -1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
