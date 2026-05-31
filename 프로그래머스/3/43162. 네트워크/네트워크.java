ddccbda5c09ae69380aa86052d1fc88e29cf1d86
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-05-31 13:02:37 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,49 +1,29 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     
-    static ArrayList<Integer>[] adj;
-    static boolean[] visited;
-    static int cnt;
+    public static boolean[] visited;
     
-    public int solution(int n, int[][] computers) {
-        
-        adj = new ArrayList[n];
-        
-        for(int i=0; i<n; i++){
-            adj[i] = new ArrayList<>();
-        }
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i!=j && computers[i][j]==1){
-                    adj[i].add(j);
-                }
+    public static void dfs(int net, int[][] coms) {
+        for(int i=0; i<coms[net].length; i++) {
+            if(i==net) continue;
+            if(!visited[i] && coms[net][i]==1) {
+                visited[i] = true;
+                dfs(i,coms);
             }
         }
-        
-        visited = new boolean[n];
-        cnt = 0;
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                dfs(i);
-                cnt++;
-            }
-        }
-        
-        int answer = cnt;
-        return answer;
     }
     
-    private static void dfs(int i){
-        visited[i] = true;
-        for(int v : adj[i]){
-            if(!visited[v]){
-                visited[v] = true;
-                dfs(v);
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        int answer = 0;
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                dfs(i,computers);
+                answer++;
             }
         }
+        return answer;
     }
 }
