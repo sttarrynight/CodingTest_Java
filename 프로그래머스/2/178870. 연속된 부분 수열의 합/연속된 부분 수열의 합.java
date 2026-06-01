c1626253c34a8afe05657e39769ed736326cd7d3
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-06-01 12:34:31 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 6.59 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,36 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        // 시작,끝 인덱스 모두 0에서 출발
-        // 누적합 배열에서 끝인덱스 - (시작인덱스-1)와 k와 비교
-        // 그 값이 k보다 작으면 끝 인덱스를 +1
-        // 그 값이 k보다 크면 시작 인덱스를 +1
-        // 값이 k와 같으면 비교 후 길이가 짧은 인덱스 저장 및 시작 인덱스 +1 (탐색을 이어나감)
-        
-        int[] sums = new int[sequence.length];
-        sums[0] = sequence[0];
-        
-        for(int i=1; i<sums.length; i++) {
-            sums[i] = sums[i-1] + sequence[i];
-        }
+        int[] answer = {0,1000001};
         
         int start = 0;
         int end = 0;
-        int[] answer = new int[2];
-        answer[1] = 1000001;
+        int sum = 0;
         
-        while(end < sums.length) {
-            int cur = start==0 ? sums[end] : sums[end] - sums[start-1];
-            if(cur < k) {
-                end++;
-            } else if(cur > k) {
-                start++;
-            } else {
-                if(end-start < answer[1]-answer[0]) {
+        while(true) {
+            if(sum >= k) {
+                if(sum == k && end - 1 - start < answer[1] - answer[0]) {
+                    answer[1] = end - 1;
                     answer[0] = start;
-                    answer[1] = end;
                 }
-                start++;
+                sum -= sequence[start++];
+            } else {
+                if(end == sequence.length) break;
+                sum += sequence[end++];
             }
         }
         
