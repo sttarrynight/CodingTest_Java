57bc250578b599ad7159c0c74adb6179664ec0bc
KIM YOUNGBEEN <81636454+sttarrynight@users.noreply.github.com>
2026-06-17 10:19:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 64.08 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] nums = number.split("");
+        stack.push(nums[0]);
+        
+        for(int i=1; i<number.length(); i++) {
+            int now = Integer.parseInt(nums[i]);
+            int top = Integer.parseInt(stack.peek());
+            while(now > top && k > 0) {
+                stack.pop();
+                k--;
+                if(stack.isEmpty()) break;
+                top = Integer.parseInt(stack.peek());
+            }
+            stack.push(nums[i]);
+        }
+        
+        if(k-- > 0) {
+            stack.pop();
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        
+        return sb.toString();
+    }
+}
